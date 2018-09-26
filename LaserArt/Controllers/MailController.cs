e72b4a81b1a85da74919dd09fe058e4037d0e117
h2002d <davit.harutyunyan1996@gmail.com>
2018-09-26 10:41:52 +0400

    Update MailController.cs

--- LaserArt/Controllers/MailController.cs
+++ LaserArt/Controllers/MailController.cs
@@ -17,8 +17,8 @@ namespace LaserArt.Controllers
         public ActionResult SendMail(int orderId)
         {
             MailMessage mail = new MailMessage();
-            mail.To.Add("davit.harutyunyan1996@gmail.com");
-            mail.From = new MailAddress("softsonicip@gmail.com");
+            mail.To.Add("");
+            mail.From = new MailAddress("");
             mail.Subject = "Order";
             string Body = "Hello";
             mail.Body = Body;
@@ -27,7 +27,7 @@ namespace LaserArt.Controllers
             smtp.Host = "smtp.gmail.com";
             smtp.Port = 587;
             smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new System.Net.NetworkCredential("softsonicip@gmail.com", "d341044*"); // Enter seders User name and password   
+            smtp.Credentials = new System.Net.NetworkCredential("", ""); // Enter seders User name and password   
             smtp.EnableSsl = true;
             smtp.Send(mail);
             return Json("Картинка загружена", JsonRequestBehavior.AllowGet);
